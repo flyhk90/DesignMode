6d5e8daf78ad9b22f814416f97ca017b5dfbb53e
flyhk90 <flyhk90@qq.com>
2021-12-15 12:50:35 +0800

    1:测试数据库

---
+++ DesignCreate/Covariant.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignCreate
+{
+    public class Covariant
+    {
+
+        /// <summary>
+        /// 协变_逆变_不变
+        /// </summary>
+        public void Test()
+        {
+            //协变 输出参数，子类当作父类返回。
+            IEnumerable<string> list = new List<string>() { "1", "2", "3" };
+            IEnumerable<object> list2 = list;
+
+            //不变
+            //IList<string> list3 = new List<string>() { "1", "2" };
+            //IList<object> list4 = (IList<object>)list3;
+            //list4.Add(new object());
+            //Console.WriteLine(list4[2]);
+
+            //逆变 输入参数，父类转成子类传入
+            Action<object> objectAction = (obj) => Console.WriteLine(obj);
+            Action<string> stringAction = objectAction;
+            stringAction("string");
+
+
+            //  ArrayList  1：数据类型不安全，比如计算。2：效率偏低，有装箱操作
+            ArrayList al = new ArrayList();
+            al.Add(1);
+            al.Add("何康");
+            //int test = (int)al[1];
+            //Console.WriteLine(test);
+        }
+    }
+}
--- DesignCreate/Program.cs
+++ DesignCreate/Program.cs
@@ -1,4 +1,81 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //创建型设计模式 简单来说就是用来创建对象的
+using System.Reflection;
 Console.WriteLine("你好,github我来了！！！flyhk90");
 Console.WriteLine("vs2022,真香！");
+
+
+Cat cat = new();
+Person person = new Person();
+Mouse mouse = new Mouse();
+
+cat.tz += person.getUp;
+cat.tz += mouse.runHouse;
+cat.startSoud();
+
+#region 观察者模型
+/// <summary>
+/// 猫叫
+/// </summary>
+public class Cat
+{
+    public Action tz;
+    public void startSoud()
+    {
+        Console.WriteLine("猫叫了！");
+        //通知事件
+        if (tz != null) tz();
+    }
+}
+
+public class Person
+{
+    public void getUp()
+    {
+        Console.WriteLine("主人起床了");
+    }
+}
+public class Mouse
+{
+    public void runHouse()
+    {
+        Console.WriteLine("老鼠跑了！");
+    }
+}
+#endregion
+
+
+
+
+#region .NET6入门 语法
+//var CurrentMethod = MethodBase.GetCurrentMethod();
+//Console.WriteLine($"Method Name:{CurrentMethod?.Name}");
+//Console.WriteLine($"Class Name:{CurrentMethod?.DeclaringType?.Name}");
+//Console.WriteLine($"Namespace Name:{CurrentMethod?.DeclaringType?.Namespace}");
+//Console.WriteLine($"Assembly Name:{CurrentMethod?.DeclaringType?.Assembly.FullName}");
+//Console.WriteLine($"Module Name:{CurrentMethod?.DeclaringType?.Module.Name}");
+
+//Console.WriteLine(getName());
+//Console.WriteLine(TestA.getName());
+//Console.WriteLine(new TestB().getName());
+
+//public static string getName()
+//{
+//    return "何康";
+//}
+public class TestA
+{
+    public static string getName()
+    {
+        return "Class_Static何康1";
+    }
+}
+public class TestB
+{
+    public  string getName()
+    {
+        return "Class_Static何康2";
+    }
+}
+#endregion
+
